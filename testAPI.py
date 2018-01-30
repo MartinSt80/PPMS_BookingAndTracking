92319fad29237df6a271076e541670fb1ab4a33a
MartinTh80 <martin.stoeckl80@googlemail.com>
2018-01-30 16:54:34 +0100

    updates including error handling

--- testAPI.py
+++ testAPI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
-from lib import Options, PPMSAPICalls
+from lib import Options, PPMSAPICalls, Errors
 
 SYSTEM_OPTIONS = Options.OptionReader('SystemOptions.txt')
 
@@ -10,8 +10,11 @@ system_id = SYSTEM_OPTIONS.getValue('PPMS_systemid')
 calling_mode = SYSTEM_OPTIONS.getValue('calling_mode')
 
 get_systemname = PPMSAPICalls.NewCall(calling_mode)
-system_name = get_systemname.getSystemName(system_id)
-print system_name
+try:
+	system_name = get_systemname.getSystemName(system_id)
+	print system_name
+except Errors.APIError as e:
+	print e.msg
 
 get_booking = PPMSAPICalls.NewCall(calling_mode)
 print get_booking.getTodaysBookings(facility_id, system_name)
@@ -21,23 +24,29 @@ user_name = get_username.getUserFullName('martin.stoeckl')
 print user_name
 
 get_userexp = PPMSAPICalls.NewCall(calling_mode)
-print get_userexp.getExperience('martin.stoeckl', system_id)
+try:
+	print get_userexp.getExperience('martin.stoeckl', system_id)
+except Errors.APIError as e:
+	print e.msg
 
 get_userid = PPMSAPICalls.NewCall(calling_mode)
 user_id = get_userid.getUserID(user_name, facility_id)
 print user_id
 
-# make_booking = PPMSAPICalls.NewCall(calling_mode)
-# make_booking.makeBooking('2017-08-22T18:00:00', '2017-08-22T20:00:00', '2017-08-04T17:00:00', user_id, system_id, facility_id)
+make_booking = PPMSAPICalls.NewCall(calling_mode)
 
-get_groups = PPMSAPICalls.NewCall(calling_mode)
-grouplist = get_groups.getGroupList()
-for group in grouplist:
-	get_group = PPMSAPICalls.NewCall(calling_mode)
-	print get_group.getGroupPI(group)
-	get_user = PPMSAPICalls.NewCall(calling_mode)
-	users = get_group.getGroupUsers(group)
-	print users
+try:
+	print make_booking.makeBooking('2018-01-29T18:00:00', '2018-01-29T20:00:00', '2018-01-29T15:00:00', user_id, system_id, facility_id)
+
+except Errors.APIError as e:
+	print e.msg
 
 
-
+# get_groups = PPMSAPICalls.NewCall(calling_mode)
+# grouplist = get_groups.getGroupList()
+# for group in grouplist:
+# 	get_group = PPMSAPICalls.NewCall(calling_mode)
+# 	print get_group.getGroupPI(group)
+# 	get_user = PPMSAPICalls.NewCall(calling_mode)
+# 	users = get_group.getGroupUsers(group)
+# 	print users
